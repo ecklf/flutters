cb9bc65725ba1e6db08ce4823c27e019e5e354be
ecklf <ecklf@icloud.com>
2021-05-10 11:44:59 +0200

    build: update to flutter v2
    Closes #24

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,8 +2,7 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:audioplayers/audioplayers_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -11,8 +10,8 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  AudioplayersPlugin.registerWith(registry.registrarFor(AudioplayersPlugin));
-  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  AudioplayersPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
